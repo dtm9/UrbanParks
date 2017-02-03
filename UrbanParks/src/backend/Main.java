3636ac94d5f346dd24f5430ea2c407e3cb0d0034
Dylan Miller <dtm9@uw.edu>
2017-02-03 11:49:49 -0800

    Cleaned up Main.java

--- UrbanParks/src/backend/Main.java
+++ UrbanParks/src/backend/Main.java
@@ -1,10 +1,9 @@
 package backend;
-//test this is not required for check in 2
+//This is not required for check in 2
 public class Main {
 
 	public static void main(final String[] args) {
 		// TODO init method to start up the program, log in the user, confirm account type and run the right view.
-      System.out.println("it still works");
-      OfficeStaff test = new OfficeStaff("Jim", "5551112222", "Jim");
+		System.out.println("yup still works.");
 	}
 }

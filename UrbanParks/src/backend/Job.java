3d9b81c11ed2f8caf1d819181fbb5a16a44fe835
VG-Gnome <gardnerg@uw.edu>
2017-02-03 13:35:18 -0800

    Fixed spacing for the job class, i hope i got anything.

--- UrbanParks/src/backend/Job.java
+++ UrbanParks/src/backend/Job.java
@@ -3,7 +3,7 @@ package backend;
 import java.util.ArrayList;
 /**
  *
- * @author VG Gnome
+ * @author Gardner Gomes
  * @version 1.0
  * Date 2/1/2017
  * Job Class for Urban Parks
@@ -54,157 +54,176 @@ public class Job {
      * Constructor for a Job object.
      */
     public Job() {
-		setMyDate("");
-		setMyTime("");
-		setMyLocation("");
-		setMyDescription("");
-		setMyMaxVolunteers(0);
-		setMyMinLight(0);
-		setMyMinMedium(0);
-		setMyMinHeavy(0);
-		setMyNotes("");
-		myVolunteers = new ArrayList<String>();
-
-	}
+        setMyDate("");
+        setMyTime("");
+        setMyLocation("");
+        setMyDescription("");
+        setMyMaxVolunteers(0);
+        setMyMinLight(0);
+        setMyMinMedium(0);
+        setMyMinHeavy(0);
+        setMyNotes("");
+        myVolunteers = new ArrayList<String>();
+    }
     /**
-	 * Getter for myDate.
-	 * @return myDate
-	 */
-	public String getMyDate() {
-		return myDate;
-	}
+     * Getter for myDate.
+     * @return myDate
+     * @author Gardner Gomes
+     */
+    public String getMyDate() {
+        return myDate;
+    }
     /**
-	 * Setter for myDate.
-	 * @param theDate
-	 */
-	public void setMyDate(final String theDate) {
-		this.myDate = theDate;
-	}
+     * Setter for myDate.
+     * @param theDate
+     * @author Gardner Gomes
+     */
+    public void setMyDate(final String theDate) {
+        this.myDate = theDate;
+    }
     /**
-	 * Getter for myTime.
-	 * @return myTime
-	 */
-	public String getMyTime() {
-		return myTime;
-	}
+     * Getter for myTime.
+     * @return myTime
+     * @author Gardner Gomes
+     */
+    public String getMyTime() {
+        return myTime;
+    }
     /**
-	 * Setter for myTime.
-	 * @param theTime
-	 */
-	public void setMyTime(final String theTime) {
-		this.myTime = theTime;
-	}
+     * Setter for myTime.
+     * @param theTime
+     * @author Gardner Gomes
+     */
+    public void setMyTime(final String theTime) {
+        this.myTime = theTime;
+    }
 	/**
-	 * Getter for Location.
-	 * @return myLocation
-	 */
-	public String getMyLocation() {
-		return myLocation;
-	}
+     * Getter for Location.
+     * @return myLocation
+     * @author Gardner Gomes
+     */
+    public String getMyLocation() {
+        return myLocation;
+    }
     /**
-	 * Setter for myLocation.
-	 * @param myLocation
-	 */
-	public void setMyLocation(final String theLocation/*Park thePark */) {
-		this.myLocation = theLocation;
-	}
+     * Setter for myLocation.
+     * @param myLocation
+     * @author Gardner Gomes
+     */
+    public void setMyLocation(final String theLocation/*Park thePark */) {
+        this.myLocation = theLocation;
+    }
     /**
-	 * Getter for myDescription.
-	 * @return
-	 */
-	public String getMyDescription() {
-		return myDescription;
-	}
+     * Getter for myDescription.
+     * @return myDescription
+     * @author Gardner Gomes
+     */
+    public String getMyDescription() {
+        return myDescription;
+    }
     /**
-	 * Setter for myDescription.
-	 * @param myDescription
+     * Setter for myDescription.
+     * @param myDescription
+     * @author Gardner Gomes
 	 */
-	public void setMyDescription(final String theDescription) {
-		this.myDescription = theDescription;
-	}
+    public void setMyDescription(final String theDescription) {
+        this.myDescription = theDescription;
+    }
     /**
 	 * Getter for myMaxVolunteers.
 	 * @return myMaxVolunteers
+	 * @author Gardner Gomes
 	 */
-	public int getMyMaxVolunteers() {
-		return myMaxVolunteers;
-	}
+    public int getMyMaxVolunteers() {
+        return myMaxVolunteers;
+    }
     /**
 	 * Setter for myMaxVolunteers.
 	 * @param myMaxVolunteers
+	 * @author Gardner Gomes
 	 */
-	public void setMyMaxVolunteers(final int theMaxVolunteers) {
-		this.myMaxVolunteers = theMaxVolunteers;
-	}
+    public void setMyMaxVolunteers(final int theMaxVolunteers) {
+        this.myMaxVolunteers = theMaxVolunteers;
+    }
     /**
 	 * Getter for myMinLight.
 	 * @return myMinLight
+	 * @author Gardner Gomes
 	 */
-	public int getMyMinLight() {
-		return myMinLight;
-	}
+    public int getMyMinLight() {
+        return myMinLight;
+    }
     /**
-	 * Setter for myMinLight.
+     * Setter for myMinLight.
 	 * @param myMinLight
+	 * @author Gardner Gomes
 	 */
-	public void setMyMinLight(final int theMinLight) {
-		this.myMinLight = theMinLight;
-	}
+    public void setMyMinLight(final int theMinLight) {
+        this.myMinLight = theMinLight;
+    }
     /**
-	 * Getter for myMinMedium.
-	 * @return myMinMedium
-	 */
-	public int getMyMinMedium() {
-		return myMinMedium;
-	}
+     * Getter for myMinMedium.
+     * @return myMinMedium
+     * @author Gardner Gomes
+     */
+    public int getMyMinMedium() {
+        return myMinMedium;
+    }
     /**
-	 * Setter for myMinMedium.
-	 * @param myMinMedium
-	 */
-	public void setMyMinMedium(final int theMinMedium) {
-		this.myMinMedium = theMinMedium;
-	}
+     * Setter for myMinMedium.
+     * @param myMinMedium
+     * @author Gardner Gomes
+     */
+    public void setMyMinMedium(final int theMinMedium) {
+        this.myMinMedium = theMinMedium;
+    }
     /**
-	 * Getter for myMinHeavy.
-	 * @return myMinHeavy
-	 */
-	public int getMyMinHeavy() {
-		return myMinHeavy;
-	}
+     * Getter for myMinHeavy.
+     * @return myMinHeavy
+     * @author Gardner Gomes
+     */
+    public int getMyMinHeavy() {
+        return myMinHeavy;
+    }
     /**
-	 * Setter for MyMinHeavy.
-	 * @param myMinHeavy
-	 */
-	public void setMyMinHeavy(final int theMinHeavy) {
-		this.myMinHeavy = theMinHeavy;
-	}
+     * Setter for MyMinHeavy.
+     * @param myMinHeavy
+     * @author Gardner Gomes
+     */
+    public void setMyMinHeavy(final int theMinHeavy) {
+        this.myMinHeavy = theMinHeavy;
+    }
     /**
-	 * Getter for myVolunteers.
-	 * @return myVolunteers
-	 */
-	public ArrayList<String> getMyVolunteers() {
-		return myVolunteers;
-	}
+     * Getter for myVolunteers.
+     * @return myVolunteers
+     * @author Gardner Gomes
+     */
+    public ArrayList<String> getMyVolunteers() {
+        return myVolunteers;
+    }
     /**
-	 * Add a Volunteer.
-	 * @param myVolunteers
-	 */
-	public void setMyVolunteers(final String theVolunteer) {
-		this.myVolunteers.add(theVolunteer);
-	}
+     * Add a Volunteer.
+     * @param myVolunteers
+     * @author Gardner Gomes
+     */
+    public void setMyVolunteers(final String theVolunteer) {
+        this.myVolunteers.add(theVolunteer);
+    }
     /**
-	 * Getter for myNotes.
-	 * @return myNotes
-	 */
-	public String getMyNotes() {
-		return myNotes;
-	}
+     * Getter for myNotes.
+     * @return myNotes
+     * @author Gardner Gomes
+     */
+    public String getMyNotes() {
+        return myNotes;
+    }
     /**
-	 * Setter for myNotes.
-	 * @param theNotes
-	 */
-	public void setMyNotes(final String theNotes) {
-		this.myNotes = theNotes;
-	}
+     * Setter for myNotes.
+     * @param theNotes
+     * @author Gardner Gomes
+     */
+    public void setMyNotes(final String theNotes) {
+        this.myNotes = theNotes;
+    }
 
 }

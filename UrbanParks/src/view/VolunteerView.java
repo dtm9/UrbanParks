c4e25e1898620048adab4ab4831f857ef57de844
Ethan Young <young.e.f42@gmail.com>
2017-02-13 19:13:01 -0800

    Finished viewing jobs for volunteers.

--- UrbanParks/src/view/VolunteerView.java
+++ UrbanParks/src/view/VolunteerView.java
@@ -3,15 +3,137 @@
  */
 package view;
 
+import backend.Datastore;
+import backend.OfficeStaff;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import backend.Account;
+import backend.Job;
+import backend.Volunteer;
+import backend.Park;
+
 /**
  * @author dylan
  *
  */
 public class VolunteerView extends View {
-
+	/** OS-independent string to add a line break in string builder. */
+    private static final String SB_LINE_BREAK = System.getProperty("line.separator");
+    private Volunteer myVolunteer;
+    private final Datastore myDatastore;
+    private Scanner myScanner = new Scanner(System.in);
+    private StringBuilder mySB;
+    
+    public VolunteerView(Account theAccount, Datastore theDatastore){
+    	super();
+    	myVolunteer = (Volunteer) theAccount;
+    	myDatastore = theDatastore;
+    	
+    	
+    }
 	@Override
 	public void launchGUI() {
 		// TODO Auto-generated method stub
+		mainMenu();
+	}
+	
+	private void mainMenu(){
+		 	mySB.append("\nWelcome to Urban Parks\nVolunteer: ");
+	        mySB.append(myVolunteer.getRealName());
+	        mySB.append("\n----------------------------------------------------------\n\n");
+	        mySB.append("1. Volunteer for jobs\n");
+	        mySB.append("2. View My Jobs\n");
+	        mySB.append("3. Exit\n");
+	        mySB.append("\n\nPlease Select a number followed by the enter key: ");
+	        System.out.print(mySB.toString());
+	        mySB.delete(0, mySB.capacity());
+	        int theChoice = myScanner.nextInt();
+	        
+	        switch (theChoice) {
+	        case 1:
+	            signUpForJob();
+	            break;
+	        case 2:
+	            viewJobs();
+	            break;
+	        case 3:
+	            break;
+	        }
+	}
+	
+	private void signUpForJob(){
+		mySB.append("Which park do you want to volunteer for?");
+	    mySB.append("\n----------------------------------------------------------\n\n");
+	    List<Park> parkList = myDatastore.getAllParks();
+	    Iterator<Park> itr = parkList.iterator();
+	    int count = 0;
+	    if(parkList==null) mySB.append("There are no jobs to sign up for");
+	    while(itr.hasNext()){
+	    	Park currentPark =itr.next();
+	    	count++;
+	    	mySB.append(count);
+	    	mySB.append(". ");
+	    	mySB.append(currentPark.getName());
+	    	mySB.append("\n");
+	    }
+	    count ++;
+	    mySB.append(count);
+	    mySB.append(". Back to main menu\n");
+	    System.out.print(mySB.toString());
+	    mySB.delete(0, mySB.capacity());
+	    int theChoice = myScanner.nextInt();
+	    if (theChoice==count){
+	    	mainMenu();
+	    } else{
+	    	Park selectedPark = parkList.get(theChoice);
+	    	List<Job> parkJobList = myDatastore.getJobsByPark(selectedPark);
+	    	listJobs(selectedPark, parkJobList);
+	    }
+	    
+	    
+	}
+	
+	private void listJobs(Park selectedPark, List<Job> parkJobList){
+		mySB.append("Which job do you want to sign up for?\n");
+		mySB.append("\n----------------------------------------------------------\n\n");
+		
+		
+	}
+	
+	private void viewJobs(){
+		mySB.append("\nView Jobs for: ");
+		mySB.append(myVolunteer.getRealName());
+		mySB.append("\n----------------------------------------------------------\n\n");
+		List<Job> volunteerJobs = myDatastore.getJobsByVolunteer(myVolunteer);
+		Iterator<Job> itr = volunteerJobs.iterator();
+		int count =0;
+		if (volunteerJobs==null) mySB.append("You have not signed up for any jobs");
+		while( itr.hasNext()){
+			 Job currentJob = itr.next();
+			 count++;
+			 mySB.append(count);
+			 mySB.append(". ");
+			 mySB.append(currentJob.getName());
+			 mySB.append("\n");
+			 
+		}	
+		mySB.append("\n1. Back");
+		mySB.append("\n2. Exit\n");
+		System.out.print(mySB.toString());
+		mySB.delete(0, mySB.capacity());
+		int theChoice = myScanner.nextInt();
+		
+		switch (theChoice) {
+        case 1:
+            mainMenu();
+            break;
+        case 2:
+            break;
+        
+        }
 		
 	}
 

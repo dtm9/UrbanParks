d328b86d11cf158e84db8a68b060f845172d2411
Dylan Miller <dtm9@uw.edu>
2017-02-17 02:11:46 -0800

    Several depreciated instance fields removed from volunteer

--- UrbanParks/src/model/Volunteer.java
+++ UrbanParks/src/model/Volunteer.java
@@ -18,50 +18,18 @@ public class Volunteer extends AbstractAccount implements Serializable {
   /** Default serialVersionUID for serialization. */
   private static final long serialVersionUID = 1L;
 
-/**
-   * Level at which a volunteer can perform a job.
-   * @author Dylan Miller
-   */
-  public enum WorkGrade {
-    /**Lowest skill level.*/
-    LIGHT,
-    /**Middle skill level.*/
-    MEDIUM,
-    /**Highest skill level.*/
-    HEAVY
-  }
-
-  /**Blacklisted status of the Volunteer.*/
-  private boolean blackballed;
-
-  /**Skill level of the Volunteer.*/
-  private WorkGrade myWorkGrade;
-
-  /**Number of times volunteer did not show up for a job.*/
-  private int myAbsenceCount;
-
-  /**Number of times volunteer cancelled too close to the job date.*/
-  private int myBadCancellationCount;
-
-  /**Linked List of notes added to volunteer's record by the staff.*/
-  private LinkedList<String> myAccountNotes;
-
   /**
    * Creates an Volunteer user. minimal permissions
    * @param theUsername email address of the user that is used
    * to log into the system.
    * @param thePhone phone number of the user.
    * @param theRealName legal name of the user.
-   * @param theWorkGrade the skill level of the user.
    * @author Dylan Miller
    */
-  public Volunteer(final String theUsername, final String thePhone,
-                   final String theRealName, final WorkGrade theWorkGrade) {
+  public Volunteer(final String theUsername, final String thePhone, final String theRealName) {
     super(theUsername, thePhone, theRealName);
-    myAccountNotes = new LinkedList<String>();
-    this.myWorkGrade = theWorkGrade;
   }
-
+  //TODO can volunteer just have a 3 arg constructor?
   /**
    * Creates a volunteer user. minimal permissions.
    * Volunteers do not need to provide all of the account info at first
@@ -72,9 +40,8 @@ public class Volunteer extends AbstractAccount implements Serializable {
    * @param theWorkGrade skill level of the user.
    * @author Dylan Miller
    */
-  public Volunteer(final String theUsername, final String theRealName,
-  final WorkGrade theWorkGrade) {
-    this(theUsername, null, theRealName, theWorkGrade);
+  public Volunteer(final String theUsername, final String theRealName) {
+    this(theUsername, null, theRealName);
   }
 
   /**
@@ -86,114 +53,8 @@ public class Volunteer extends AbstractAccount implements Serializable {
    * @param theWorkGrade skill level of the volunteer.
    * @author Dylan Miller
    */
-  public Volunteer(final String theUsername, final WorkGrade theWorkGrade) {
-    this(theUsername, null, null, theWorkGrade);
-  }
-
-  /**
-  * Checks if the user is blacklisted by Urban Parks.
-  * @return the myIsBlackballed boolean value for user's blacklisted status.
-  * @author Dylan Miller
-  */
-  public boolean isBlackballed() {
-    return blackballed;
-  }
-
-  /**
-  * Blacklists the volunteer user.
-  * @param theBlackballStatus boolean value for user's
-  * blacklisted status to set.
-  * @author Dylan Miller
-  */
-  public void setBlackballed(final boolean theBlackballStatus) {
-    this.blackballed = theBlackballStatus;
-  }
-
-  /**
-  * Returns the work grade currently set for the volunteer.
-  * @return the myWorkGrade enum representing the grade.
-  * @author Dylan Miller
-  */
-  public WorkGrade getWorkGrade() { //TODO consider using an enum for this
-    return myWorkGrade;
-  }
-
-  /**
-  * Updates the volutneer's work grade to the value provided.
-  * @param theWorkGrade enum representing the grade.
-  * @author Dylan Miller
-  */
-  public void setWorkGrade(final WorkGrade theWorkGrade) {
-    this.myWorkGrade = theWorkGrade;
-  }
-
-  /**
-  * Returns the number of times this volunteer has not
-  * shown up to a job they signed up for.
-  * @return the myAbsenceCount number of times abandoning jobs.
-  * @author Dylan Miller
-  */
-  public int getAbsenceCount() {
-    return myAbsenceCount;
-  }
-
-  /**
-  * Updates the absent count for this volunteer.
-  * @param theAbsenceCount number of times abandoning jobs to set.
-  * @author Dylan Miller
-  * @author Ethan Young
-  */
-  public void setAbsenceCount(final int theAbsenceCount) {
-	  if (theAbsenceCount<0){
-		  throw new IllegalArgumentException("Parameter can't be less than 0");
-	  } else {
-		  this.myAbsenceCount = theAbsenceCount;
-	  }
-  }
-
-  /**
-  * Returns the number of times this volunteer
-  * cancelled in the violation period.
-  * @return the myBadCancellationCount the number of times cancelled
-  * too close to the job date.
-  * @author Dylan Miller
-  */
-  public int getBadCancellationCount() {
-    return myBadCancellationCount;
-  }
-
-  /**
-  * Updates the bad cancellation count for this volunteer.
-  * @param theBadCancellationCount number of of times cancelled
-  * too close to the job date to set.
-  * @author Dylan Miller
-  * @author Ethan Young
-  */
-  public void setBadCancellationCount(final int theBadCancellationCount) {
-	  if (theBadCancellationCount<0){
-		  throw new IllegalArgumentException("Parameter can't be less than 0");
-	  } else {
-		  this.myBadCancellationCount = theBadCancellationCount;
-	  }
-  }
-
-  /**
-   * Adds a new note onto the volunteer.
-   *
-   * @param theNewNote any string to be added to the account notes.
-   * @author Dylan Miller
-   * @author Ethan Young
-   * @author Walter Weeks
-   */
-  public void addAccountNote(final String theNewNote) {
-      if (theNewNote == null ) {
-        throw new NullPointerException("theNewNote cannot be null.");
-      }
-      if (theNewNote.equals("")) {
-        throw new IllegalArgumentException("theNewNote cannot be empty.");
-      }
-
-      this.myAccountNotes.add(theNewNote);
+  public Volunteer(final String theUsername) {
+    this(theUsername, null, null);
   }
   
   //TODO new method. Test this and remove the old one in datastore.java

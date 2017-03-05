1a23833434ce8004a3baf12a05b6d096a2821ff8
LittleWalter <ww3@uw.edu>
2017-03-05 10:24:47 -0800

    VolunteerViewTest: Removed tabs in source

--- UrbanParks/src/view/VolunteerViewTest.java
+++ UrbanParks/src/view/VolunteerViewTest.java
@@ -23,70 +23,70 @@ import model.Volunteer;
  * @author Peter Park (ragnarok@uw.edu)
  */
 public class VolunteerViewTest {
-	
-	private Volunteer myVolunteer;
-	private ParkManager myParkManager;
-	private VolunteerView myView;
+    
+    private Volunteer myVolunteer;
+    private ParkManager myParkManager;
+    private VolunteerView myView;
     private Datastore myDatastore;
-	private Park myPark;
-	private StringBuilder mySB;
-
-	/**
-	 * Sets up the text fixtures.
-	 * @author Peter Park
-	 */
-	@Before
-	public void setUp() throws Exception {
-		myDatastore = new Datastore();
-		myVolunteer = new Volunteer("peterpark@gmail.com", "5551112222", "Peter Park");
-		myParkManager = new ParkManager("batman@tacomaparks.com", "5551112222", "Dylan Miller");
-	    myPark = new Park(myParkManager, "Franklin Park", "1201 S Puget Sound Ave", "Tacoma", "WA", "98408");
-		myView = new VolunteerView(myVolunteer);
-		mySB = new StringBuilder();
-		
-		myDatastore.addAccount(myVolunteer);
-		myDatastore.addAccount(myParkManager);
-	}
-	
-	/**
+    private Park myPark;
+    private StringBuilder mySB;
+    
+    /**
+     * Sets up the text fixtures.
+     * @author Peter Park
+     */
+    @Before
+    public void setUp() throws Exception {
+        myDatastore = new Datastore();
+        myVolunteer = new Volunteer("peterpark@gmail.com", "5551112222", "Peter Park");
+        myParkManager = new ParkManager("batman@tacomaparks.com", "5551112222", "Dylan Miller");
+        myPark = new Park(myParkManager, "Franklin Park", "1201 S Puget Sound Ave", "Tacoma", "WA", "98408");
+        myView = new VolunteerView(myVolunteer);
+        mySB = new StringBuilder();
+        
+        myDatastore.addAccount(myVolunteer);
+        myDatastore.addAccount(myParkManager);
+    }
+    
+    /**
      * Tests for a valid/good job passed in.
      * @author Peter Park
      */
     @Test
     public void minDaysAway_GoodJob_ListThisJob() {
-		ZoneId z = ZoneId.of("America/Los_Angeles");
-		LocalDate today = LocalDate.now(z);
-		today = today.plusDays(2); // 2 days away
-    	Job testJob1 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 1","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
-    	myDatastore.addJob(testJob1);
+        ZoneId z = ZoneId.of("America/Los_Angeles");
+        LocalDate today = LocalDate.now(z);
+        today = today.plusDays(2); // 2 days away
+        Job testJob1 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 1","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
+        myDatastore.addJob(testJob1);
         assertTrue(myView.minDaysAway(testJob1));
     }
 
-	/**
+    /**
      * Tests for a job that starts sooner than the minimum number of days away.
      * @author Peter Park
      */
     @Test
     public void minDaysAway_BadJob_DoNotListThisJob() {
-		ZoneId z = ZoneId.of("America/Los_Angeles");
-		LocalDate today = LocalDate.now(z);
-		today = today.plusDays(1); // 1 day away
-    	Job testJob2 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 2","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
-    	myDatastore.addJob(testJob2);
+        ZoneId z = ZoneId.of("America/Los_Angeles");
+        LocalDate today = LocalDate.now(z);
+        today = today.plusDays(1); // 1 day away
+        Job testJob2 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 2","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
+        myDatastore.addJob(testJob2);
         assertFalse("This will fail", myView.minDaysAway(testJob2));
     }
     
-	/**
+    /**
      * Tests for a past job passed in.
      * @author Peter Park
      */
     @Test
     public void minDaysAway_PastJob_DoNotListThisJob() {
-		ZoneId z = ZoneId.of("America/Los_Angeles");
-		LocalDate today = LocalDate.now(z);
-		today = today.minusDays(1); // 1 day in the past
-    	Job testJob3 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 3","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
-    	myDatastore.addJob(testJob3);
+        ZoneId z = ZoneId.of("America/Los_Angeles");
+        LocalDate today = LocalDate.now(z);
+        today = today.minusDays(1); // 1 day in the past
+        Job testJob3 = new Job(myPark, "10:00", "Test VolunteerViewTest Case 3","Testing", 1, today.getDayOfMonth(), today.getMonthValue(), today.getYear());
+        myDatastore.addJob(testJob3);
         assertFalse("This will fail", myView.minDaysAway(testJob3));
     }
     /**
@@ -94,11 +94,11 @@ public class VolunteerViewTest {
      * @author Ethan Young
      */
     public void isSameDayJob_SameDay_ReturnFalseFlag(){
-    	Job testJob4 = new Job(myPark, "11:00", "Test VolunteerViewTest case 4", "testing", 1, 23,3,2017);
-    	Job testJob5 = new Job(myPark, "11:00", "Test VolunteerViewTest case 5", "testing2", 1, 23,3,2017);
-    	List<Job> jobList = new ArrayList<Job>();
-    	jobList.add(testJob4);
-    	assertFalse("This will fail", myView.isSameDayJob(testJob5.getDay(), testJob5.getMonth(), jobList, mySB));
+        Job testJob4 = new Job(myPark, "11:00", "Test VolunteerViewTest case 4", "testing", 1, 23,3,2017);
+        Job testJob5 = new Job(myPark, "11:00", "Test VolunteerViewTest case 5", "testing2", 1, 23,3,2017);
+        List<Job> jobList = new ArrayList<Job>();
+        jobList.add(testJob4);
+        assertFalse("This will fail", myView.isSameDayJob(testJob5.getDay(), testJob5.getMonth(), jobList, mySB));
     }
     
     /**
@@ -106,10 +106,10 @@ public class VolunteerViewTest {
      * @author Ethan Young
      */
     public void isSameDayJob_NotSameDay_ReturnTrueFlag(){
-    	Job testJob6 = new Job(myPark, "11:00", "Test VolunteerViewTest case 4", "testing", 1, 23,3,2017);
-    	Job testJob7 = new Job(myPark, "11:00", "Test VolunteerViewTest case 5", "testing2", 1, 24,3,2017);
-    	List<Job> jobList = new ArrayList<Job>();
-    	jobList.add(testJob6);
-    	assertTrue("This will be true", myView.isSameDayJob(testJob7.getDay(), testJob7.getMonth(), jobList, mySB));
+        Job testJob6 = new Job(myPark, "11:00", "Test VolunteerViewTest case 4", "testing", 1, 23,3,2017);
+        Job testJob7 = new Job(myPark, "11:00", "Test VolunteerViewTest case 5", "testing2", 1, 24,3,2017);
+        List<Job> jobList = new ArrayList<Job>();
+        jobList.add(testJob6);
+        assertTrue("This will be true", myView.isSameDayJob(testJob7.getDay(), testJob7.getMonth(), jobList, mySB));
     }
 }
